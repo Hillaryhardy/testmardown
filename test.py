42f229608e15ac10608816efb2779d88b9864ec7
hillary <hillary>
2022-04-16 18:16:39 +0300

    mardown test

---
+++ test.py
@@ -0,0 +1,14 @@
+def print_full_name(a, b):
+
+    # What's Your Name? in Python - HackerRank Solution START
+    b = b+"!"
+    print("Hello",a, b,"You just delved into python.");
+    # What's Your Name? in Python - HackerRank Solution END
+    
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name
+print('Hello, world!')
+name=input('Whats ur name, Sir?
+)
